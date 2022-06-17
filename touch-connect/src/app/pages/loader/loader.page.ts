44454b2bc8dc2436fc51e1a69b8c51cc8d5e7caf
Th-Black-Owl <sabelo1011@gmail.com>
2022-06-17 21:13:15 +0200

    made a loadoing  crescent, styled the crescent, began changes on the loader.pages.ts file

--- touch-connect/src/app/pages/loader/loader.page.ts
+++ touch-connect/src/app/pages/loader/loader.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-loader',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoaderPage implements OnInit {
 
-  constructor() { }
+  constructor(public loadingController: LoadingController) { }
 
   ngOnInit() {
   }
