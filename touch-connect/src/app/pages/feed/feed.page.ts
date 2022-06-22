df65f24c7f6d94b504c3ea8097fe84ac98c58682
Th-Black-Owl <sabelo1011@gmail.com>
2022-06-22 22:57:32 +0200

    changes

--- touch-connect/src/app/pages/feed/feed.page.ts
+++ touch-connect/src/app/pages/feed/feed.page.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import SwiperCore, { Autoplay, Keyboard, Pagination, Scrollbar, Zoom } from 'swiper';
+
 import { IonicSlides } from '@ionic/angular';
 
-SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom, IonicSlides]);
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.page.html',
