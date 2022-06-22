b87d49723472f6611c7b46a9afa7ff824b6295db
Th-Black-Owl <sabelo1011@gmail.com>
2022-06-22 14:40:45 +0200

    changes

--- touch-connect/src/app/app-routing.module.ts
+++ touch-connect/src/app/app-routing.module.ts
@@ -26,6 +26,14 @@ const routes: Routes = [
   {
     path: 'news',
     loadChildren: () => import('./pages/news/news.module').then( m => m.NewsPageModule)
+  },
+  {
+    path: 'chat',
+    loadChildren: () => import('./pages/chat/chat.module').then( m => m.ChatPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
   }
 ];
 
