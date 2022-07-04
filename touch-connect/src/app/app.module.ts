318cdb4640aafb8168d360a23a844bc0120e2a00
Th-Black-Owl <sabelo1011@gmail.com>
2022-07-04 11:58:19 +0200

    added the authentication and chat service

--- touch-connect/src/app/app.module.ts
+++ touch-connect/src/app/app.module.ts
@@ -9,17 +9,25 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { LoadingComponent } from './components/loading/loading.component';
-import { environment } from 'src/environments/environment';
 
-import { AngularFireModule } from 'angularfire2';
+//firbase related modules
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import {AngularFireStorageModule} from '@angular/fire/compat/storage';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent, LoadingComponent],
   imports: [BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase),
     IonicModule.forRoot(),
      AppRoutingModule,
      HttpClientModule,
+     AngularFireModule.initializeApp(environment.firebase),
+     AngularFirestoreModule,
+     AngularFireStorageModule,
+     AngularFireAuthModule
     ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
